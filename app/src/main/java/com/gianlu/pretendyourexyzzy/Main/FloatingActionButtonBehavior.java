378049343c45a0ff781d5fb2886f4b3f2f1065ce
Gianlu <altomanigianluca@gmail.com>
2018-07-02 15:00:57 +0200

    Minor

--- app/src/main/java/com/gianlu/pretendyourexyzzy/Main/FloatingActionButtonBehavior.java
+++ app/src/main/java/com/gianlu/pretendyourexyzzy/Main/FloatingActionButtonBehavior.java
@@ -4,41 +4,44 @@ import android.content.Context;
 import android.support.annotation.Keep;
 import android.support.annotation.NonNull;
 import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
 import android.view.View;
 
 @Keep
-@SuppressWarnings("unused")
-public class FloatingActionButtonBehavior extends CoordinatorLayout.Behavior {
+public class FloatingActionButtonBehavior extends CoordinatorLayout.Behavior<FloatingActionButton> {
+    private static final int DURATION = 150;
+
     public FloatingActionButtonBehavior(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
 
-    private static void kickOut(View fab) {
+    private static void hide(FloatingActionButton fab) {
         fab.setTag(false);
-        fab.animate().translationY(fab.getHeight() * 2).setDuration(150).start();
+        fab.animate().scaleX(0).scaleY(0).setDuration(DURATION).start();
     }
 
-    private static void kickIn(View fab) {
+    private static void show(FloatingActionButton fab) {
         fab.setTag(true);
-        fab.animate().translationY(0).setDuration(150).start();
+        fab.animate().scaleX(1).scaleY(1).setDuration(DURATION).start();
     }
 
     @Override
-    public boolean layoutDependsOn(CoordinatorLayout parent, View child, View dependency) {
+    public boolean layoutDependsOn(CoordinatorLayout parent, FloatingActionButton child, View dependency) {
         return dependency instanceof RecyclerView;
     }
 
     @Override
-    public boolean onStartNestedScroll(@NonNull CoordinatorLayout coordinatorLayout, @NonNull View child, @NonNull View directTargetChild, @NonNull View target, int axes, int type) {
+    public boolean onStartNestedScroll(@NonNull CoordinatorLayout coordinatorLayout, @NonNull FloatingActionButton child, @NonNull View directTargetChild, @NonNull View target, int axes, int type) {
         return true;
     }
 
+
     @Override
-    public void onNestedScroll(@NonNull CoordinatorLayout coordinatorLayout, @NonNull View child, @NonNull View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed, int type) {
+    public void onNestedScroll(@NonNull CoordinatorLayout coordinatorLayout, @NonNull FloatingActionButton child, @NonNull View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed, int type) {
         if (child.getTag() == null) child.setTag(true);
-        if (dyConsumed > 0 && ((boolean) child.getTag())) kickOut(child);
-        else if (dyConsumed < 0 && (!(boolean) child.getTag())) kickIn(child);
+        if (dyConsumed > 0 && ((boolean) child.getTag())) hide(child);
+        else if (dyConsumed < 0 && (!(boolean) child.getTag())) show(child);
     }
 }
