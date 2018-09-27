38f5d89532d2a873e5d38c107d59fc719ca37910
Gianlu <altomanigianluca@gmail.com>
2018-09-27 18:46:45 +0200

    Added open source URL

--- app/src/main/java/com/gianlu/pretendyourexyzzy/PreferenceActivity.java
+++ app/src/main/java/com/gianlu/pretendyourexyzzy/PreferenceActivity.java
@@ -45,7 +45,7 @@ public class PreferenceActivity extends BasePreferenceActivity {
     @Nullable
     @Override
     protected String getOpenSourceUrl() {
-        return null;
+        return "https://github.com/devgianlu/PretendYoureXyzzyAndroid";
     }
 
     @Override
