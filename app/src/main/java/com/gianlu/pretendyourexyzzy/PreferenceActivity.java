6e0afa72c5aa19464fd3d7286374a8fef13e0632
Gianlu <altomanigianluca@gmail.com>
2018-11-14 22:20:17 +0100

    Added dark theme toggler (#6)

--- app/src/main/java/com/gianlu/pretendyourexyzzy/PreferenceActivity.java
+++ app/src/main/java/com/gianlu/pretendyourexyzzy/PreferenceActivity.java
@@ -1,13 +1,15 @@
 package com.gianlu.pretendyourexyzzy;
 
+import android.content.Context;
 import android.content.Intent;
 
 import com.danielstone.materialaboutlibrary.items.MaterialAboutActionItem;
 import com.danielstone.materialaboutlibrary.items.MaterialAboutItem;
-import com.danielstone.materialaboutlibrary.items.MaterialAboutItemOnClickAction;
 import com.gianlu.commonutils.Preferences.BasePreferenceActivity;
+import com.gianlu.commonutils.Preferences.BasePreferenceFragment;
 import com.gianlu.commonutils.Preferences.MaterialAboutPreferenceItem;
 import com.gianlu.pretendyourexyzzy.SpareActivities.TutorialActivity;
+import com.yarolegovich.mp.MaterialCheckboxPreference;
 
 import java.util.Collections;
 import java.util.List;
@@ -19,7 +21,7 @@ public class PreferenceActivity extends BasePreferenceActivity {
     @NonNull
     @Override
     protected List<MaterialAboutPreferenceItem> getPreferencesItems() {
-        return Collections.emptyList();
+        return Collections.singletonList(new MaterialAboutPreferenceItem(R.string.general, R.drawable.baseline_settings_24, GeneralFragment.class));
     }
 
     @Override
@@ -30,12 +32,7 @@ public class PreferenceActivity extends BasePreferenceActivity {
     @NonNull
     @Override
     protected List<MaterialAboutItem> customizeTutorialCard() {
-        return Collections.<MaterialAboutItem>singletonList(new MaterialAboutActionItem(R.string.showBeginnerTutorial, R.string.showBeginnerTutorial_summary, 0, new MaterialAboutItemOnClickAction() {
-            @Override
-            public void onClick() {
-                startActivity(new Intent(PreferenceActivity.this, TutorialActivity.class));
-            }
-        }));
+        return Collections.singletonList(new MaterialAboutActionItem(R.string.showBeginnerTutorial, R.string.showBeginnerTutorial_summary, 0, () -> startActivity(new Intent(PreferenceActivity.this, TutorialActivity.class))));
     }
 
     @Override
@@ -52,5 +49,24 @@ public class PreferenceActivity extends BasePreferenceActivity {
     @Override
     protected boolean disablePayPalOnGooglePlay() {
         return true;
+    }
+
+    public static class GeneralFragment extends BasePreferenceFragment {
+
+        @Override
+        protected void buildPreferences(@NonNull Context context) {
+            MaterialCheckboxPreference nightMode = new MaterialCheckboxPreference.Builder(context)
+                    .defaultValue(PK.NIGHT_MODE.fallback())
+                    .key(PK.NIGHT_MODE.key())
+                    .build();
+            nightMode.setTitle(R.string.prefs_nightMode);
+            nightMode.setSummary(R.string.prefs_nightMode_summary);
+            addPreference(nightMode);
+        }
+
+        @Override
+        public int getTitleRes() {
+            return R.string.general;
+        }
     }
 }
