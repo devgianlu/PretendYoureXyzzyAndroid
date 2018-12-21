devgianlu/PretendYoureXyzzyAndroid

commit 39ecaf7fe1b3a83a6894845ee8587167eabc76b2

Fixed "could not find active fragment -1"

app/src/main/java/com/gianlu/pretendyourexyzzy/MainActivity.java
@@ -68,6 +68,12 @@ public class MainActivity extends ActivityWithDialog implements GamesFragment.On
         if (drawerManager != null) drawerManager.syncTogglerState();
     }
 
+    @Nullable
+    @Override
+    public Object getLastNonConfigurationInstance() {
+        return null;
+    }
+
     @Override
     protected void onPostCreate(@Nullable Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);

commit 877e30399a4ff71f9c268569c7a5046d3e368988

Fixed #23 (broken by 39ecaf7fe1b3a83a6894845ee8587167eabc76b2)

app/src/main/java/com/gianlu/pretendyourexyzzy/MainActivity.java
@@ -68,12 +68,6 @@ public class MainActivity extends ActivityWithDialog implements GamesFragment.On
         if (drawerManager != null) drawerManager.syncTogglerState();
     }
 
-    @Nullable
-    @Override
-    public Object getLastNonConfigurationInstance() {
-        return null;
-    }
-
     @Override
     protected void onPostCreate(@Nullable Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);